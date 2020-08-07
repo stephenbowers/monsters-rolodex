768b0cf2463f9cc0f7d32deaf4f7d83927762266
Stephen Bowers <srb4887@gmail.com>
2020-08-06 20:52:22 -0700

    Updates

---
+++ precache-manifest.776185dd2c556aefa02f3cca34e26745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1c462a73e014710c3e2d479fe071f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cce0820b5521dec132d0",
+    "url": "/monsters-rolodex/static/css/main.613d2e9d.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce0820b5521dec132d0",
+    "url": "/monsters-rolodex/static/js/main.f99ab388.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
